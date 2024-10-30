e6486b794650495461ea84b5a7ee56bda0cfd0f0
Ekaterina Romanova <Dekaterina83@mail.ru>
2024-10-30 10:27:19 +0300

    gitignore changed

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         int startingAmount = 200; //начальная сумма счета
-        int replenishmentamount = 1000; //сумма пополнения
+        int replenishmentamount = 1500; //сумма пополнения
         int bonus;
         if (replenishmentamount > 1000) {
             bonus = replenishmentamount / 100;
